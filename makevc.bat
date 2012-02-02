a26b70cd4fc78592245bf53ee78a7be9cd67d106
Luca Spiller <luca@stackednotion.com>
2012-02-02 17:41:22 +0000

    Remove makevc file

--- makevc.bat
+++
@@ -1,4 +0,0 @@
-rem replace C:\Program Files\erl5.7.3\erts-5.7.3\include with your latest version of erlang
-
-cl /MT /LD /O2 /Ox /Ot /Fegeoip.dll /I"C:\Program Files\erl5.7.3\erts-5.7.3\include" GeoIP.c geoiperl.cpp ws2_32.lib
-pause
